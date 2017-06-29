91a61c95cbb7c0671b12df5f352030953eb27203
Ashish Verma <ashish1500616@gmail.com>
2017-06-29 11:11:32 +0530

    Add approve comment and post list

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,20 @@ from . import views
 
 urlpatterns = [
     url(r'^about/$', views.AboutView.as_view(), name="about_view"),
-    url(r'^$', views.PostListView.as_view(), name='about'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.CreatePostView.as_view, name="create_post"),
     url(r'^post/(?P<pk>\d+)/edit/$',
         views.PostrUpdateView.as_view(), name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$',
         views.PostDeleteView.as_view(), name='delete_view'),
-    url(r'^drafts/$',views.DraftListView.as_view(),name="draft")
+    url(r'^drafts/$', views.DraftListView.as_view(), name="draft"),
+    url(r'^post/(<?P<pk>\d+)/comment/$',
+        views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(<?P<pk>\d+)/approve/$',
+        views.comment_approve, name='comment_approve'),
+    url(r'^comment/(<?P<pk>\d+)/remove/$',
+        views.comment_remove, name='comment_remove'),
+    url(r'^post/(<?P<pk>\d+)/publish/$',
+        views.post_publish, name='post_publish'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.urls import reverse_lazy
-
+from django.utils import timezone
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, UpdateView, DeleteView,DetailView,CreateView
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, DetailView, CreateView
 
 
 class AboutView(TemplateView):
@@ -23,8 +23,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.object.filter(published_date__lte=timezone.now()).order_by('-published_date '
-                )
+        return Post.object.filter(published_date__lte=timezone.now()).order_by('-published_date ')
 
 
 class PostDetailView(DetailView):
@@ -61,8 +60,46 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_query(self):
         return Post.objects.filter(published_date_isnull=True).order_by('created_date'
-                )
+                                                                        )
 
 
+######################################################
+######################################################
 
-			
+
+@login_required
+def post_publish(request, pk):
+    post = get_objecct_or_404(Post, pk=pk)
+    post.published_date
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_objecct_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+        return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_objecct_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request):
+    comment = get_objecct_or_404(Comment, pk=pk)
+    # savinng post primary key as comment.delete will delte it from the a
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
